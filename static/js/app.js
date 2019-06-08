30d38a0c1a31507416694503bbc127ba7487c8ad
matthewellishanson <lifeasmeh@gmail.com>
2019-06-07 21:02:47 -0500

    append table using javascript

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// from data.js
+var tableData = data;
+
+// set variable for tbody
+var tbody = d3.select("tbody")
+
+tableData.forEach((alienSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(alienSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
